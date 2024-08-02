00abc64483faf9cc49c9920555c3e64d353a068c
Reyn <reynbargaso@gmail.com>
2024-08-02 23:45:59 +0800

    node update

---
+++ index.js
@@ -0,0 +1,13 @@
+const { exec } = require('child_process');
+
+exec('bash start.sh', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing start.sh: ${error}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
